bceef5dcb663588edf3b9bc4073030e5d10de597
martinrcruz <martinignacioriosc@gmail.com>
2022-06-02 18:30:31 -0400

    Subida de primer modulo, insertando y leyendo info de la bd

--- application/modules/agenda/controllers/Agenda.php
+++ application/modules/agenda/controllers/Agenda.php
@@ -29,4 +29,107 @@ class Agenda extends CI_Controller
         }
     }
 
+
+    public function getHorasAgenda()
+    {
+        $response = new stdClass();
+        $response->data = [];
+
+        if ($query = $this->agenda_model->getHorasAgenda()) {
+            foreach ($query->result() as $res) {
+                $row = null;
+                $row = new stdClass();
+                $row->id_hora_agenda = $res->id_hora_agenda;
+                $row->id_servicio = $res->id_servicio;
+                $row->nombre = $res->nombre;
+                $row->descripcion = $res->descripcion;
+                $row->fecha_entrada = $res->fecha_entrada;
+                $row->fecha_salida = $res->fecha_salida;
+                $row->id_usuario_tecnico = $res->id_usuario_tecnico;
+                $row->id_cliente = $res->id_cliente;
+                $row->id_vehiculo = $res->id_vehiculo;
+                $row->id_usuario_cargo = $res->id_usuario_cargo;
+                $row->fecha_creacion = $res->fecha_creacion;
+
+                array_push($response->data, $row);
+            }
+        }
+        echo json_encode($response);
+    }
+
+
+    public function getHorasAgendaById($id = '')
+    {
+        $response = new stdClass();
+        $response->data = [];
+        $where = '';
+
+        $id ? $where = " AND id_hora_agenda=$id" : $where = '';
+        // if($id){
+        //     $where = " AND id_hora_agenda=$id";
+        // }
+
+        if ($query = $this->agenda_model->getHorasAgenda($where)) {
+            foreach ($query->result() as $res) {
+                $row = null;
+                $row = new stdClass();
+                $row->id_hora_agenda = $res->id_hora_agenda;
+                $row->id_servicio = $res->id_servicio;
+                $row->nombre = $res->nombre;
+                $row->descripcion = $res->descripcion;
+                $row->fecha_entrada = $res->fecha_entrada;
+                $row->fecha_salida = $res->fecha_salida;
+                $row->id_usuario_tecnico = $res->id_usuario_tecnico;
+                $row->id_cliente = $res->id_cliente;
+                $row->id_vehiculo = $res->id_vehiculo;
+                $row->id_usuario_cargo = $res->id_usuario_cargo;
+
+                array_push($response->data, $row);
+            }
+        }
+        echo json_encode($response);
+    }
+
+    public function insertHoraAgenda()
+    {
+        //DECLARACION DE VARIABLES, OBJETOS Y ARRAYS
+        $response = new stdClass();
+        $response->id = null;
+        $response->data = [];
+        $response->proceso = 0;
+        $response->errores = [];
+        $id_agenda_hora = '';// REPRESENTA EL IDENTIFICADOR DE HORA_AGENDA
+        $datos = [];
+
+        //LOGICA DE NEGOCIO Y REQUEST/RESPONSE DE API
+        $datos = array(
+            'id_servicio' => 1,
+            'nombre' => 'Mantencion numero 2',
+            'descripcion' => 'Mantencion preventiva Toyota corolla',
+            'fecha_entrada' => '2022-06-01',
+            'fecha_salida' => '2022-06-04',
+            'id_usuario_tecnico' => 1,
+            'id_cliente' => 1,
+            'id_vehiculo' => 1,
+            'id_usuario_cargo' => 1,
+            'fecha_creacion' => date('Y-m-d H:s'),
+            'estado' => 1,
+        );
+
+        //INSERCION, ACTUALIZACION U OPERACIONES
+        if ($id_agenda_hora) {
+            if ($query = $this->agenda_model->updateAgendaHora('hora_agenda', 'id_hora_agenda', $datos, $id_agenda_hora)) {
+                $response->proceso = 1;
+                $response->id = $query;
+                $response->data = $datos;
+            }
+        } else {
+            if ($query = $this->agenda_model->insertHoraAgenda('hora_agenda', $datos)) {
+                $response->proceso = 1;
+                $response->id = $query;
+                $response->data = $datos;
+            }
+        }
+        print_r($response);
+    }
 }
--- application/modules/agenda/models/Agenda_model.php
+++ application/modules/agenda/models/Agenda_model.php
@@ -6,67 +6,35 @@ class Agenda_model extends CI_Model
     {
         parent::__construct();
     }
-    public function getDataActividad($where = "", $clauses = [], $orderby = "", $limit = "")
+    public function getHorasAgenda($where='')
     {
-        $sql = "SELECT 
-        A.ID_ACTIVIDAD AS ID,
-        A.CAMPO AS ACTIVIDAD_NOMBRE
-        FROM ACTIVIDAD A
-            WHERE A.ESTADO=1 $where
-            $orderby $limit";
-
-        $query = $this->db->query($sql, $clauses);
+        $sql = "SELECT * FROM hora_agenda WHERE ESTADO=1 $where;";
+        $query = $this->db->query($sql);
         //var_dump($this->db->last_query());
+
         if ($query->num_rows() > 0)
             return $query;
         else
             return false;
     }
-    public function getActividad($where = "", $clauses = [])
+
+    public function insertHoraAgenda($tabla, $data)
     {
-        $sql = "SELECT 
-        A.ID_ACTIVIDAD AS ID,
-        A.CAMPO AS ACTIVIDAD_NOMBRE
-        FROM ACTIVIDAD A
-            WHERE A.ESTADO=1 $where";
-
-        $query = $this->db->query($sql, $clauses);
-        //print_r($this->db->last_query());
-        if ($query->num_rows() > 0)
-            return $query;
-        else
-            return false;
+        $query = $this->db->insert($tabla, $data);
+		if ($query)
+			return $this->db->insert_id(); 
+		else
+			return false;
     }
-
-    public function insertar_actividad_area($id_area, $id_actividad, $fecha)
+    public function updateHoraAgenda($tabla, $comparar, $datos, $id)
     {
-        $sql = "INSERT INTO ACTIVIDAD_AREA (ID_AREA, ID_ACTIVIDAD, FECHACREACION, ESTADO) VALUES ($id_area, $id_actividad, $fecha, 1)";
-        $query = $this->db->query($sql);
-        //print_r($this->db->last_query());
-        if ($query->num_rows() > 0)
-            return $query;
-        else
-            return false;
+    	$this->db->where($comparar, $id);
+		$result = $this->db->update($tabla, $datos);
+		if ($result)
+			return true;
+		else
+			return false;
     }
-
-    public function get_area($id_actividad)
-    {
-        $sql = "SELECT 
-        A.ID_AREA AS ID_AREA,
-        A.CAMPO AS NOMBRE_AREA,
-        AA.ID_ACT_AREA AS ID_ACTIVIDAD_AREA
-        FROM AREA A
-        LEFT JOIN ACTIVIDAD_AREA AA ON AA.ID_AREA=A.ID_AREA AND AA.ESTADO=1
-            WHERE A.ESTADO=1 $id_actividad GROUP BY AA.ID_AREA";
-        $query = $this->db->query($sql);
-        //print_r($this->db->last_query());
-
-        if ($query->num_rows() > 0)
-            return $query;
-        else
-            return false;
-    }
-
 
 
 }
